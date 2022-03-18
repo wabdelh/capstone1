f0a75b2b0105904fcf45f7f8dfb349437fd1040c
huynggg <huyanguyen3695@gmail.com>
2022-03-17 20:02:45 -0500

    - Connect Firebase - Basic register activities

--- capstone/src/main/java/com/example/capstone/RegisterUser.kt
+++ capstone/src/main/java/com/example/capstone/RegisterUser.kt
@@ -3,13 +3,45 @@ package com.example.capstone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterUser : AppCompatActivity(), View.OnClickListener {
+    //private TextView registerUser
+    //private EditText editTextFirst, editTextLast, exitTextEmail, editTextPassword
+
+    private lateinit var registerUser: TextView
+    private lateinit var editTextFirst: EditText
+    private lateinit var editTextLast: EditText
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextPassword: EditText
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_user)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // [END initialize_auth]
+
+        // register button to save user input to the database
+        registerUser = findViewById<Button>(R.id.register)
+        registerUser.setOnClickListener(this)
+
+        editTextFirst = findViewById<EditText>(R.id.firstName)
+        editTextLast = findViewById<EditText>(R.id.lastName)
+        editTextEmail = findViewById<EditText>(R.id.emailR)
+        editTextPassword = findViewById<EditText>(R.id.passwordR)
+
 
         findViewById<FloatingActionButton>(R.id.backToLogin).setOnClickListener(this)
         //backToLogin in R.id.backToLogin is the id of the view object (the back button),
@@ -24,6 +56,55 @@ class RegisterUser : AppCompatActivity(), View.OnClickListener {
                 //with the name of the page to move to, MainActivity is the activity to move to
                 startActivity(Intent(this, MainActivity::class.java))
             }
+            R.id.register -> {
+                registerUser()
+            }
+        }
+    }
+
+    fun registerUser() {
+        var email: String = editTextEmail.text.toString().trim()
+        var firstName: String = editTextFirst.text.toString().trim()
+        var lastName: String = editTextLast.text.toString().trim()
+        var password: String = editTextPassword.text.toString().trim()
+
+        if(firstName.isEmpty()){
+            editTextFirst.setError("First name is required!")
+            editTextFirst.requestFocus()
+            return
+
+        }
+
+        if(lastName.isEmpty()){
+            editTextLast.setError("Last name is required!")
+            editTextLast.requestFocus()
+            return
+
+        }
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required!")
+            editTextEmail.requestFocus()
+            return
+
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide a valid email!")
+            editTextEmail.requestFocus()
+            return
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!")
+            editTextPassword.requestFocus()
+            return
+
+        }
+
+        if(password.length < 6){
+            editTextPassword.setError("Please enter a longer password!")
+            editTextPassword.requestFocus()
+            return
+
         }
     }
 }
