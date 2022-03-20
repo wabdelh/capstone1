b317cf69c4bbc49719278be0e4245a4f6cff20ab
mbertuccimobile <michaelbertucci@my.unt.edu>
2022-03-20 16:40:25 -0500

    Fixed login and user verification
    fixed login and user verification, will now display proper error message and redirect user.

--- capstone/src/main/java/com/example/capstone/AfterLoginHome.kt
+++ capstone/src/main/java/com/example/capstone/AfterLoginHome.kt
@@ -1,11 +1,29 @@
 package com.example.capstone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class AfterLoginHome : AppCompatActivity() {
+class AfterLoginHome : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after_login_home)
+        findViewById<Button>(R.id.logout).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.logout -> {
+                // Firebase.auth.signOut()
+                //println("1")
+                // startActivity(Intent(this, MainActivity::class.java))
+            }
+        }
     }
 }
--- capstone/src/main/java/com/example/capstone/MainActivity.kt
+++ capstone/src/main/java/com/example/capstone/MainActivity.kt
@@ -14,16 +14,35 @@ import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var progressBar: ProgressBar
+    lateinit var loginUser: Button
+    lateinit var editEmail: EditText
+    lateinit var editPassword: EditText
+
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.register).setOnClickListener(this)
         findViewById<TextView>(R.id.login).setOnClickListener(this)
-        //register in R.id.register is the id of the view object (the register button),
-        //the <TextView> casts the view to whatever kind of object it is,
-        //in this context, it is a TextView objects
-        //when this object is clicked, call onClick(v) where v is the object
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // [END initialize_auth]
+
+
+        // login button to verify user input to the database
+        loginUser = findViewById<Button>(R.id.login)
+        loginUser.setOnClickListener(this)
+
+        editEmail = findViewById<EditText>(R.id.emailL)
+        editPassword = findViewById<EditText>(R.id.passwordL)
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
     }
 
     override fun onClick(v: View) {
@@ -37,85 +56,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.login -> { //when v.id == R.id.register, the id of register object
                 //startActivity changes active activity, taking an Intent object made
                 //with the name of the page to move to, RegisterUser is the activity to move to
-                startActivity(Intent(this, ULogin::class.java))
+                loginUser()
             }
         }
     }
 
-    class ULogin : AppCompatActivity(), View.OnClickListener {
+    fun loginUser() {
 
-        private lateinit var progressBar: ProgressBar
-        private lateinit var loginUser: Button
-        private lateinit var editEmail: EditText
-        private lateinit var editPassword: EditText
+        // var for username and pw
+        var email: String = editEmail.text.toString().trim()
+        var password: String = editPassword.text.toString().trim()
 
-        private lateinit var auth: FirebaseAuth
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-
-            // Initialize Firebase Auth
-            auth = Firebase.auth
-            // [END initialize_auth]
-
-
-            // login button to verify user input to the database
-            loginUser = findViewById<Button>(R.id.login)
-            loginUser.setOnClickListener(this)
-
-            editEmail = findViewById<EditText>(R.id.emailL)
-            editPassword = findViewById<EditText>(R.id.passwordL)
-            progressBar = findViewById<ProgressBar>(R.id.progressBarR)
-
-
+        if (email.isEmpty()) {
+            editEmail.setError("Email is required!")
+            editEmail.requestFocus()
+            return
         }
 
-        override fun onClick(v: View) {
-            when (v.id) { //when is the kotlin switch statement. use the following syntax
-                R.id.login -> {
-                    loginUser()
+        if (password.isEmpty()) {
+            editPassword.setError("Password is required!")
+            editPassword.requestFocus()
+            return
+        }
+
+        progressBar.setVisibility(View.VISIBLE)
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    setContentView(R.layout.activity_after_login_home)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        baseContext, "Login failed, please confirm credentials.",
+                        Toast.LENGTH_SHORT
+
+                    ).show()
+                    progressBar.setVisibility(View.GONE)
                 }
             }
-        }
-
-        fun loginUser() {
-
-            // var for username and pw
-            var email: String = editEmail.text.toString().trim()
-            var password: String = editPassword.text.toString().trim()
-
-            if (email.isEmpty()) {
-                editEmail.setError("Email is required!")
-                editEmail.requestFocus()
-                return
-            }
-
-            if (password.isEmpty()) {
-                editPassword.setError("Password is required!")
-                editPassword.requestFocus()
-                return
-            }
-
-
-            progressBar.setVisibility(View.VISIBLE)
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        val user = auth.currentUser
-                        setContentView(R.layout.activity_after_login_home)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(
-                            baseContext, "Login failed, please confirm credentials.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-
-        }
-    }
-    fun signOut() {
-        Firebase.auth.signOut()
     }
 }
+
+
