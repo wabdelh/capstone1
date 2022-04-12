5ae2b19c58e06bdde00e5e1b03d46e5936fa800b
mbertuccimobile <michaelbertucci@my.unt.edu>
2022-04-11 19:10:24 -0500

    Beautify

--- capstone/src/main/java/com/example/capstone/Invite.kt
+++ capstone/src/main/java/com/example/capstone/Invite.kt
@@ -17,7 +17,7 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_invite.*
 
 class Invite : AppCompatActivity(), View.OnClickListener {
-    val roles = arrayOf("Primary","Secondary","Tertiary")
+    val roles = arrayOf("Primary", "Secondary", "Tertiary")
     lateinit var invite: Button
     private lateinit var iemail: EditText
     private lateinit var auth: FirebaseAuth
@@ -36,15 +36,19 @@ class Invite : AppCompatActivity(), View.OnClickListener {
 
         auth = Firebase.auth
 
-        val arrayAdapter = ArrayAdapter(this@Invite,android.R.layout.simple_spinner_dropdown_item,roles)
+        val arrayAdapter =
+            ArrayAdapter(this@Invite, android.R.layout.simple_spinner_dropdown_item, roles)
         spinner2.adapter = arrayAdapter
-        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?,
-                                        p1: View?,
-                                        p2: Int,
-                                        p3: Long) {
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                p0: AdapterView<*>?,
+                p1: View?,
+                p2: Int,
+                p3: Long
+            ) {
 
-              Toast.makeText(this@Invite,"You have selected "+roles[p2],Toast.LENGTH_LONG).show()
+                Toast.makeText(this@Invite, "You have selected " + roles[p2], Toast.LENGTH_LONG)
+                    .show()
                 num = p2
             }
 
@@ -61,101 +65,124 @@ class Invite : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
-      when(v.id) {R.id.Invite ->{
+        when (v.id) {
+            R.id.Invite -> {
 
-          // add user as primary caretaker
-          if(num == 0){
-              validEmail()
-              addPrimaryBabyEmail()
+                // add user as primary caretaker
+                if (num == 0) {
+                    validEmail()
+                    addPrimaryBabyEmail()
 
-          }
-          // add user as secondary caretaker
-          if(num == 1){
-              validEmail()
-              addSecondaryBabyEmail()
-
-
-          }
-          // add user as tertiary caretaker
-          if(num == 2){
-              validEmail()
-              addTertiaryBabyEmail()
-
-          }
-
-
-      }
-      }
-    }
-private fun validEmail(){
-    val email: String = iemail.text.toString().trim()
-    var inputInvalid = false
-
-    if (email.isEmpty()) {
-        iemail.error = "Email is required!"
-        iemail.requestFocus()
-        inputInvalid = true
-    }
-
-    if(inputInvalid)
-        return
-}
-
-    private fun addPrimaryBabyEmail(){
-        val email: String = iemail.text.toString().trim()
-        FirebaseDatabase.getInstance().getReference("Babies").child(babyKey).addListenerForSingleValueEvent(object :
-            ValueEventListener {
-            override fun onDataChange(d: DataSnapshot) {
-                val baby = d.getValue(Baby::class.java)
-
-                baby?.Primary?.add(email)
-                FirebaseDatabase.getInstance().getReference("Babies").child(babyKey).setValue(baby).addOnSuccessListener {
-                    Toast.makeText(this@Invite, "Primary Email has been added successfully!", Toast.LENGTH_LONG).show()
-                    setContentView(R.layout.activity_after_login_home)
-                }.addOnFailureListener{
-                    Toast.makeText(this@Invite, "Failed to add Email", Toast.LENGTH_LONG).show()
                 }
+                // add user as secondary caretaker
+                if (num == 1) {
+                    validEmail()
+                    addSecondaryBabyEmail()
+
+
+                }
+                // add user as tertiary caretaker
+                if (num == 2) {
+                    validEmail()
+                    addTertiaryBabyEmail()
+
+                }
+
+
             }
-            override fun onCancelled(error: DatabaseError) {}
-        })
+        }
     }
 
-    private fun addSecondaryBabyEmail(){
+    private fun validEmail() {
         val email: String = iemail.text.toString().trim()
-        FirebaseDatabase.getInstance().getReference("Babies").child(babyKey).addListenerForSingleValueEvent(object :
-            ValueEventListener {
-            override fun onDataChange(d: DataSnapshot) {
-                val baby = d.getValue(Baby::class.java)
+        var inputInvalid = false
 
-                baby?.Secondary?.add(email)
-                FirebaseDatabase.getInstance().getReference("Babies").child(babyKey).setValue(baby).addOnSuccessListener {
-                    Toast.makeText(this@Invite, "Secondary Email has been added successfully!", Toast.LENGTH_LONG).show()
-                }.addOnFailureListener{
-                    Toast.makeText(this@Invite, "Failed to add Email", Toast.LENGTH_LONG).show()
-                }
-            }
-            override fun onCancelled(error: DatabaseError) {}
-        })
+        if (email.isEmpty()) {
+            iemail.error = "Email is required!"
+            iemail.requestFocus()
+            inputInvalid = true
+        }
+
+        if (inputInvalid)
+            return
     }
 
-    private fun addTertiaryBabyEmail(){
+    private fun addPrimaryBabyEmail() {
         val email: String = iemail.text.toString().trim()
-        FirebaseDatabase.getInstance().getReference("Babies").child(babyKey).addListenerForSingleValueEvent(object :
-            ValueEventListener {
-            override fun onDataChange(d: DataSnapshot) {
-                val baby = d.getValue(Baby::class.java)
+        FirebaseDatabase.getInstance().getReference("Babies").child(babyKey)
+            .addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(d: DataSnapshot) {
+                    val baby = d.getValue(Baby::class.java)
 
-                baby?.Tertiary?.add(email)
-                FirebaseDatabase.getInstance().getReference("Babies").child(babyKey).setValue(baby).addOnSuccessListener {
-                    Toast.makeText(this@Invite, "Tertiary Email has been added successfully!", Toast.LENGTH_LONG).show()
-
-
-                }.addOnFailureListener{
-                    Toast.makeText(this@Invite, "Failed to add Email", Toast.LENGTH_LONG).show()
+                    baby?.Primary?.add(email)
+                    FirebaseDatabase.getInstance().getReference("Babies").child(babyKey)
+                        .setValue(baby).addOnSuccessListener {
+                        Toast.makeText(
+                            this@Invite,
+                            "Primary Email has been added successfully!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        setContentView(R.layout.activity_after_login_home)
+                    }.addOnFailureListener {
+                        Toast.makeText(this@Invite, "Failed to add Email", Toast.LENGTH_LONG).show()
+                    }
                 }
-            }
-            override fun onCancelled(error: DatabaseError) {}
-        })
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
+    }
+
+    private fun addSecondaryBabyEmail() {
+        val email: String = iemail.text.toString().trim()
+        FirebaseDatabase.getInstance().getReference("Babies").child(babyKey)
+            .addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(d: DataSnapshot) {
+                    val baby = d.getValue(Baby::class.java)
+
+                    baby?.Secondary?.add(email)
+                    FirebaseDatabase.getInstance().getReference("Babies").child(babyKey)
+                        .setValue(baby).addOnSuccessListener {
+                        Toast.makeText(
+                            this@Invite,
+                            "Secondary Email has been added successfully!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }.addOnFailureListener {
+                        Toast.makeText(this@Invite, "Failed to add Email", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
+    }
+
+    private fun addTertiaryBabyEmail() {
+        val email: String = iemail.text.toString().trim()
+        FirebaseDatabase.getInstance().getReference("Babies").child(babyKey)
+            .addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(d: DataSnapshot) {
+                    val baby = d.getValue(Baby::class.java)
+
+                    baby?.Tertiary?.add(email)
+                    FirebaseDatabase.getInstance().getReference("Babies").child(babyKey)
+                        .setValue(baby).addOnSuccessListener {
+                        Toast.makeText(
+                            this@Invite,
+                            "Tertiary Email has been added successfully!",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+
+                    }.addOnFailureListener {
+                        Toast.makeText(this@Invite, "Failed to add Email", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
     }
 
 }
